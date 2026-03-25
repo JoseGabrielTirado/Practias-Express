0d0ea0b132a6d494e510f989a4c937961b9f24a8
JoseGabrielTirado <tiradolopezjose.12@gmail.com>
2026-03-25 17:16:49 -0600

    Se agrego endpoint /usuario con JSON

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
-});
+app.get('/usuario', (req, res) => {
+  const usuario = {
+    id: 1,
+    nombre: 'Jose Gabriel Tirado Lopez',
+    rol: 'Estudiante de Informatica'
+  };
 
+  res.json(usuario);
+});
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
