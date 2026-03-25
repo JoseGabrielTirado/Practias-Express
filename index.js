946d72b38cb7ba365d2d3dab7be6b33174cd36e9
JoseGabrielTirado <tiradolopezjose.12@gmail.com>
2026-03-25 17:33:05 -0600

    Agregada ruta raíz y endpoint de /usuario

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('API funcionando.');
+});
+
 app.get('/usuario', (req, res) => {
   const usuario = {
     id: 1,
